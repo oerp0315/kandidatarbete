8719696fde0eba1cb75af5edac95cb25f36fb495
Jonatan Eklöv <jonatan.eklov@gmail.com>
2023-02-28 16:47:01 +0100

    Bättre namngivingin i kinetik1

--- kinetik1.jl
+++ kinetik1.jl
@@ -4,25 +4,25 @@ using DifferentialEquations, ModelingToolkit, Plots, Random, Distributions
 
 
 # Objekt för experimentresultat
-struct results
+struct experiment_results
       c0::AbstractVector
-      cslut::AbstractVector
-      ts::Number
+      c_final::AbstractVector
+      t_final::Number
 end
 
 #Funktion för att köra modellen givet parametrar och initialvärden
-function modell1simulator(θin,c0,ts)
+function modellsimulator1(θin,c0,ts)
       @parameters t θ[1:4]     #Parametrar i modellen
       @variables c1(t) c2(t) c3(t)   #Variabler i modellen
       D = Differential(t) #Definierar tecken för derivata
 
-      eqs = [D(c1) ~ -θ[1]*c1+θ[2]*c2,
+      equation_system = [D(c1) ~ -θ[1]*c1+θ[2]*c2,
             D(c2) ~ θ[1]*c1-θ[2]*c2-θ[3]*c2+θ[4]*c3,
             D(c3) ~ θ[3]*c2-θ[4]*c3]  #Uttryck för systemet som diffrentialekvationer
 
 
-      @named sys = ODESystem(eqs) #Definierar av som är systemet från diffrentialekvationerna
-      sys = structural_simplify(sys) #Skriver om systemet så det blir lösbart
+      @named system = ODESystem(equation_system) #Definierar av som är systemet från diffrentialekvationerna
+      system = structural_simplify(system) #Skriver om systemet så det blir lösbart
 
       u0 = [c1=>c0[1],
             c2=>c0[2],
@@ -34,48 +34,43 @@ function modell1simulator(θin,c0,ts)
            θ[4] =>θin[4]] #Definierar värden för parametrarna
 
       tspan = (0.0, ts) #Tiden vi kör modellen under
-      prob = ODEProblem(sys, u0, tspan, p, jac = true)  #Definierar vad som ska beräknas
+      prob = ODEProblem(system, u0, tspan, p, jac = true)  #Definierar vad som ska beräknas
       sol = solve(prob,Rodas5())  #Beräknar lösningen
       return sol
 end
 
 # Kör experiment
 function experimenter(ts,c0; θin = [1 0.5 3 10], standarddeviation=0)
-
-      sol = modell1simulator(θin,c0,ts)
-      
-      noisedistribution = Normal(0,standarddeviation)
-      cslut = sol[:,end] + rand(noisedistribution,3)
-
-      return cslut
+      sol = modellsimulator1(θin,c0,ts) #Genererar lösningar
+      noise_distribution = Normal(0,standarddeviation) #Skapar error
+      return sol[:,end] + rand(noise_distribution,3) # Lägger till error
 end
 
 
-function kostnadsfunktion(θ,Data::AbstractVector)
+function kostnadsfunktion(θ,experimental_data::AbstractVector)
       error=0
-      for data in Data
-            sol = modell1simulator(θ,data.c0,data.ts)
-            cs = sol.u[end]
-            error +=sum((cs-data.cslut).^2)
+      for data in experimental_data
+            sol = modellsimulator1(θ,data.c0,data.ts)
+            c_final_model = sol.u[end]
+            error +=sum((c_final_model-data.c_final).^2)
       end
       return error
 end
 
-function datagenerator(ts,c0)
-      data = results(c0,experimenter(ts,c0),ts)
-      return data
-end
 
-Data = []
+experimental_data = []
 for i = 1:10
-      tsdata = 30*rand()
-      c0data = [rand(),rand(),rand()]
-      data = datagenerator(tsdata,c0data)
-      push!(Data,data)
+      t_final_data = 30*rand()
+      c0_data = [rand(),rand(),rand()]
+      c_final_data=experimenter(t_final_data, c0_data)
+
+      current_data = experiment_results(c0_data,c_final_data, t_final_data)
+      push!(experimental_data, current_data)
 end
 
 
-#println(kostnadsfunktion([1,0.5,3,10],Data))
+
+#println(kostnadsfunktion([1,0.5,3,10],experimental_data))
 
 
 
