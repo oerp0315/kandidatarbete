d616e906096a8a1410190ebf4153a107b9bb664f
Ola Rosengren <oerp0003@gmail.com>
2023-03-10 13:02:53 +0100

    almost finished version

--- newton_minimize.jl
+++ newton_minimize.jl
@@ -48,11 +48,11 @@ end
 function latin_hypercube(n_samples, bounds; seed=123)
     Random.seed!(seed)
 
-    if isfile("latin_hypercube.csv")
+    n_vars = length(bounds)
+
+    if isfile("latin_hypercube.csv") & length(readdlm("latin_hypercube.csv", Float64)[1, :]) == n_vars
         return readdlm("latin_hypercube.csv", Float64)
     else
-        n_vars = length(bounds)
-
         # Initialize the Latin square as an n-by-n array of zeros
         square = zeros(Int, n_samples, n_samples)
 
@@ -96,109 +96,149 @@ function remove_zeros(v::AbstractVector)
     return filter(x -> x != 0, v)
 end
 
-function opt_alg(f::Function, bounds; max_iter=1000)
+struct log_results
+    sample_num_list::Vector{Int64}
+    x_current_sample_lists::Vector{Union{Float64,AbstractArray}}
+    x_current_iter::Vector{Union{Float64,AbstractArray}}
+    function_values::Vector{Float64}
+    term_criteria::Vector{Union{Float64,AbstractArray}}
+    term_reason::Vector{Union{Float64,String}}
+    time_log::Vector{Float64}
+end
+
+function opt(f::Function, x, sample_num, iter; max_iter=1000)
+    # initiate lists for logging results
+    sample_num_list::Vector{Int64} = zeros(max_iter)
+    x_current_sample_list::Vector{Union{Float64,AbstractArray}} = zeros(max_iter)
+    x_current_iter::Vector{Union{Float64,AbstractArray}} = zeros(max_iter)
+    function_values::Vector{float64} = zeros(max_iter)
+    term_criteria::Vector{Union{Float64,AbstractArray}} = zeros(max_iter)
+    term_reason::Vector{Union{Float64,String}} = zeros(max_iter)
+    time_log::Vector{Float64} = zeros(1)
+
+    x_current_samplepoint = x
+
+    time = @elapsed for i in 1:max_iter
+        # increment interation number used for printing current iteration number
+        iter += 1
+
+        # print sample number and interation number 
+        println("Iteration number: ", iter, ", Sample number: ", sample_num)
+
+        # Evaluate the function and its gradient and Hessian at the current point
+        grad = ForwardDiff.gradient(f, x)
+        hess = ForwardDiff.hessian(f, x)
+
+        # To compare with the current x in termination criteria 
+        x_prev = x
+
+        is_descent_direction::Bool = false
+
+        # Depending on if the hessian is positive definite or not, either newton or steepest descent is used
+        if isposdef(hess)
+            x, is_descent_direction = newton_method(f, grad, hess, x)
+        end
+
+        if !is_descent_direction
+            x, is_descent_direction = steepest_descent(f, grad, x)
+        end
+
+        if !is_descent_direction
+            println("Descent direction not found!")
+            break
+        end
+
+        # Finite termination criteria            
+        eps = 1e-3
+
+        # calculate function value used in termination criteria
+        function_value = f(x)
+
+        current_term_criteria = []
+        # termination criteria 1
+        if norm(grad) <= eps * (1 + abs(function_value))
+            push!(current_term_criteria, "1")
+        end
+        # termination criteria 2
+        if f(x_prev) - function_value <= eps * (1 + abs(function_value))
+            push!(current_term_criteria, "2")
+        end
+        # termination criteria 3
+        if norm(x_prev - x) <= eps * (1 + norm(x))
+            push!(current_term_criteria, "3")
+        end
+
+        # logging
+        sample_num_list[i] = sample_num
+        x_current_sample_list[i] = x_current_samplepoint
+        x_current_iter[i] = x_prev
+        function_values[i] = f(x_prev)
+        term_criteria[i] = current_term_criteria
+
+        if length(current_term_criteria) >= 2
+            term_reason[i] = "Two or more termination criteria was met"
+            break
+        else
+            term_reason[i] = " "
+        end
+    end
+
+    # log time for each sample
+    time_log[1] = time
+
+    res = log_results(sample_num_list,
+        x_current_sample_list,
+        x_current_iter,
+        function_values,
+        term_criteria,
+        term_reason,
+        time_log)
+
+    return res, iter
+end
+
+function p_est(f::Function, bounds, n_samples)
+    # Check if the data.csv exists and truncate it if it does
+    data_file = open("data.csv", "w")
+    if isfile("data.csv")
+        truncate(data_file, 0)
+    end
+    close(data_file)
+
+    # Check if the time_log.csv exists and truncate it if it does
+    timelog_file = open("time_log.csv", "w")
+    if isfile("time_log.csv")
+        truncate(timelog_file, 0)
+    end
+    close(timelog_file)
 
     # Generate Latin hypercube samples in the search space
-    n_samples = 100
     x_samples = latin_hypercube(n_samples, bounds)
     x_min = x_samples[1, :]
     f_min = f(x_min)
 
-    # initiate varible for iteration and sample number
-    iter = 0
+    # initiate varible sample and iteration number
     sample_num = 0
-
-    # initiate a vector for logging for time consumed to find minimum for samples
-    time_log = zeros(n_samples)
+    iter = 0
 
     for x in eachrow(x_samples)
-        # initiate lists for logging results
-        sample_num_list::Vector{Int64} = zeros(max_iter)
-        x_current_sample_list::Vector{Union{Float64,AbstractArray}} = zeros(max_iter)
-        x_current_iter::Vector{Union{Float64,AbstractArray}} = zeros(max_iter)
-        function_values = zeros(max_iter)
-        term_criteria::Vector{Union{Float64,AbstractArray}} = zeros(max_iter)
-        term_reason::Vector{Union{Float64,String}} = zeros(max_iter)
-
         sample_num += 1
-        x_current_samplepoint = x
 
-        time = @elapsed for i in 1:max_iter
-            # increment interation number used for printing current iteration number
-            iter += 1
+        # minimizes the cost function for the current start-guess
+        res, iter = opt(f::Function, x, sample_num, iter)
 
-            # print sample number and interation number 
-            println("Iteration number: ", iter, ", Sample number: ", sample_num)
-
-            # Evaluate the function and its gradient and Hessian at the current point
-            grad = ForwardDiff.gradient(f, x)
-            hess = ForwardDiff.hessian(f, x)
-
-            # To compare with the current x in termination criteria 
-            x_prev = x
-
-            is_descent_direction::Bool = false
-
-            # Depending on if the hessian is positive definite or not, either newton or steepest descent is used
-            if isposdef(hess)
-                x, is_descent_direction = newton_method(f, grad, hess, x)
-            end
-
-            if !is_descent_direction
-                x, is_descent_direction = steepest_descent(f, grad, x)
-            end
-
-            if !is_descent_direction
-                println("Descent direction not found!")
-                break
-            end
-
-            # Finite termination criteria            
-            eps = 1e-3
-
-            # calculate function value used in termination criteria
-            function_value = f(x)
-
-            current_term_criteria = []
-            # termination criteria 1
-            if norm(grad) <= eps * (1 + abs(function_value))
-                push!(current_term_criteria, "1")
-            end
-            # termination criteria 2
-            if f(x_prev) - function_value <= eps * (1 + abs(function_value))
-                push!(current_term_criteria, "2")
-            end
-            # termination criteria 3
-            if norm(x_prev - x) <= eps * (1 + norm(x))
-                push!(current_term_criteria, "3")
-            end
-
-            # logging
-            sample_num_list[i] = sample_num
-            x_current_sample_list[i] = x_current_samplepoint
-            x_current_iter[i] = x_prev
-            function_values[i] = f(x_prev)
-            term_criteria[i] = current_term_criteria
-
-            if length(current_term_criteria) >= 2
-                term_reason[i] = "Two or more termination criteria was met"
-                break
-            else
-                term_reason[i] = " "
-            end
-        end
-
-        time_log[sample_num] = time
-
-        data = DataFrame(Samplepoint=remove_zeros(sample_num_list),
-            Currentsample=remove_zeros(x_current_sample_list),
-            Iteration=remove_zeros(x_current_iter),
-            Functionvalues=remove_zeros(function_values),
-            Terminationcriteria=remove_zeros(term_criteria),
-            Terminationreason=remove_zeros(term_reason))
+        data = DataFrame(Samplepoint=remove_zeros(res.sample_num_list),
+            Currentsample=remove_zeros(res.x_current_sample_list),
+            Iteration=remove_zeros(res.x_current_iter),
+            Functionvalues=remove_zeros(res.function_values),
+            Terminationcriteria=remove_zeros(res.term_criteria),
+            Terminationreason=remove_zeros(res.term_reason))
 
         # modifying the content of myfile.csv using write method
         CSV.write("data.csv", data; append=true)
+
+        # log time for each sample point
+        CSV.write("time_log.csv", DataFrame(time=res.time_log); append=true)
 
         # Update the minimum point and value
         f_val = f(x)
@@ -208,25 +248,15 @@ function opt_alg(f::Function, bounds; max_iter=1000)
         end
     end
 
-    return x_min, f_min, time_log
-end
-
-function run_opt_alg()
-    # Define the function to optimize
-    f(x) = cost_function(problem_object, x, experimental_data)
-
-    # Define bounds
-    bounds = [(0, 11), (0, 11), (0, 11), (0, 11)]
-
-    # Find the minimum point and value among the samples
-    min_point, min_val, time_log = opt_alg(f, bounds)
-
-    # log time for each sample point
-    CSV.write("time_log.csv", DataFrame(time=time_log))
-
     # Print the results
-    println("Minimum point: ", min_point)
-    println("Minimum value: ", min_val)
+    println("Minimum point: ", x_min)
+    println("Minimum value: ", f_min)
 end
 
-run_opt_alg()
+# Define the function to optimize
+f(x) = cost_function(problem_object, x, experimental_data)
+
+# Define bounds
+bounds = [(0, 11), (0, 11), (0, 11), (0, 11)]
+
+p_est(f, bounds, 100)
