15ea8ac812cba2a5bf424a089957bb1a61fafb34
Jonatan Eklöv <jonatan.eklov@gmail.com>
2023-03-22 15:07:01 +0100

    Lagt till mRNA kinetik

--- kinetics_equations.jl
+++ kinetics_equations.jl
@@ -1,6 +1,6 @@
 function model_initialize()
-    @parameters t Extracellular_glucose k_{t,Snf3} k_{t,Snf3} k_{a,Snf3} k_{i,Snf3g} k_{t,Std1} k_{d,Std1}  k_{a,Std1:Rgt1} k_{i,Std1:Rgt1} k_{t,Mth1} k_{d,Mth1} k_{a,Mth1:Rgt1} k_{i,Mth1:Rgt1}
-    @variables  Snf3(t) Snf3g(t) Std1(t) Std1:Rgt1(t) Mth1(t) Mth1:Rgt1(t) Mth1:Rgt1(t) mSNF3(t) mSTD1(t) mMTH1(t) mRGT1(t) #Variabler i modellen
+    @parameters t Extracellular_glucose k_{t,Snf3} k_{t,Snf3} k_{a,Snf3} k_{i,Snf3g} k_{t,Std1} k_{d,Std1}  k_{a,Std1:Rgt1} k_{i,Std1:Rgt1} k_{t,Mth1} k_{d,Mth1} k_{a,Mth1:Rgt1} k_{i,Mth1:Rgt1}  V_{mSNF3} θ_{Mig1-Snf3} θ_{Mig2-Snf3} V_{mSTD1} θ_{Rgt1_active-Snf3} V_{mMTH1} θ_{Rgt1_active-MTH1} θ_{Mig1-MTH1} θ_{Mig2-MTH1} V_{mRGT1}
+    @variables  Snf3(t) Snf3g(t) Std1(t) Std1:Rgt1(t) Mth1(t) Mth1:Rgt1(t) Mth1:Rgt1(t)  Rgt1_active(t) mSNF3(t) mSTD1(t) mMTH1(t) mRGT1(t) #Variabler i modellen
     D = Differential(t)
 
     equation_system = [D(Snf3) ~ k_{t,Snf3}*mSNF3- k_{d,Snf3}*Snf3 - k_{a,Snf3}*Snf3*Extracellular_glucose + k_{i,Snf3g}*Snf3g,
@@ -11,11 +11,12 @@ function model_initialize()
         D(Std1:Rgt1) ~ k_{a,Std1:Rgt1} *Std1*Rgt1 - k_{i,Std1:Rgt1}*Std1:Rgt1,
         D(Mth1:Rgt1) ~ k_{a,Mth1:Rgt1} *Mth1*Rgt1 - k_{i,Mth1:Rgt1}*Mth1:Rgt1,
 
+        Rgt1_active ~ Std1:Rgt1 + Mth1:Rgt1,
 
-        D(mSNF3) ~ -k_{t,mSNF3}*mSNF3 + 1,
-        D(mSTD1) ~ -k_{t,mSTD1}*mSTD1 + 1,
-        D(mMTH1) ~ -k_{t,mMTH1}*mMTH1 + 1,
-        D(mRGT1) ~ -k_{t,mRGT1}*mRGT1 +1]
+        D(mSNF3) ~ -k_{t,mSNF3}*mSNF3 + V_{mSNF3}/(1+θ_{Mig1-Snf3}*Mig1)/(1+θ_{Mig2-Snf3}*Mig2),
+        D(mSTD1) ~ -k_{t,mSTD1}*mSTD1 + V_{mSTD1}/(1+θ_{Rgt1_active-Snf3}*Rgt1_active),
+        D(mMTH1) ~ -k_{t,mMTH1}*mMTH1 + V_{mMTH1}/(1+θ_{Rgt1_active-MTH1}*Rgt1_active)/(1+θ_{Mig1-MTH1}*Mig1)/(1+θ_{Mig2-MTH1}*Mig2),
+        D(mRGT1) ~ -k_{t,mRGT1}*mRGT1 +V_{mRGT1}]
 
     @named system = ODESystem(equation_system) #Definierar av som är systemet från diffrentialekvationerna
     system = structural_simplify(system) #Skriver om systemet så det blir lösbart
