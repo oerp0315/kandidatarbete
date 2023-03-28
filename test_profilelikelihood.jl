9917f23656237672f78818daf5b588f9138bced1
Ola Rosengren <oerp0003@gmail.com>
2023-03-28 16:59:18 +0200

    changes to profilelikelihood

--- test_profilelikelihood.jl
+++ test_profilelikelihood.jl
@@ -1,17 +1,58 @@
 using Optim
 include("kinetics_calculator.jl")
+include("newton_minimize.jl")
+
+function step_size(param_last, param_index; threshold=0.025, q=0.1)
+    step_size = zeros(length(param_last))
+    step_size[param_index] = 10
+    for i in 1:50
+        if abs(f(param_last + step_size) - f(param_last) - q*threshold) < 1e-3
+            break
+        else
+            step_size[param_index] /= 2
+        end
+    end
+
+    return param_last .+ step_size
+end
+
+function pl_costfunction_calc(x_hat, fix_index, fix_value)
+    x = insert!(x_hat, fix_index, fix_value)
+    pl_calc = cost_function(problem_object, x, experimental_data)
+
+    return pl_calc
+end
+
+function intermediate_cost_function(x_small, index_x_small, x_big)
+    _x_big = convert.(eltype(x_small), x_big)
+    _x_big[index_x_small] .= x_small
+    return cost_function(problem_object, _x_big, experimental_data)
+end
 
 # Define the function to perform profile likelihood analysis for one parameter
-function profile_likelihood(params, data, param_index, num_points, threshold)
+function profile_likelihood(params, data, param_index, bounds, num_points, threshold)
+
     # Save the optimized values for the other three parameters
     other_params = [params[i] for i in 1:4 if i != param_index]
 
-    # Define the function to optimize with respect to the parameter of interest
-    objective(param_value) = -likelihood([other_params; param_value], data)
+    # list of indexes to be optimized
+    index_list = [i for i in 1:4 if i != param_index]
+
+    # new bounds
+    new_bounds = deleteat!(bounds, param_index)
+
+    # new start values
+    x_samples = readdlm("latin_hypercube.csv", Float64)
+    new_x_samples = hcat(bounds[:, 1:param_index-1], A[:, param_index+1:end])
+
+    # Omdefinera när det behövs
+    cost_function_profilelikelihood = (x) -> intermediate_cost_function(x, index_list, params)
+
+    for
+
 
     # Find the maximum likelihood estimate for the parameter of interest
-    result = optimize(objective, [params[param_index]], method=GoldenSection())
-    mle = result.minimizer[1]
+    x_min, f_min = p_est(cost_function_profilelikelihood, new_bounds, 100)
 
     # Find the maximum likelihood estimates for the other three parameters at each value of the parameter of interest
     profile_likelihood_values = []
